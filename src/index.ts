4cdf63e991170de30716b228b23407d2d23a1afd
DaniLaurs <sousal22@outlook.com>
2025-12-03 16:18:39 -0300

    adicionando rota raiz

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.get("/", (req, res) => {
+  res.send("API funcionando 🚀");
+});
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/plans", planRoutes);
