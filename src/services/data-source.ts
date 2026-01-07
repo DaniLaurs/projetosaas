110ee2c1bc0bea5d91aec4cdc39e840feec65a7a
DaniLaurs <sousal22@outlook.com>
2026-01-07 17:32:46 -0300

    fix: force ipv4 connection for supabase on render

--- src/services/data-source.ts
+++ src/services/data-source.ts
@@ -8,17 +8,23 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
+
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
+
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 
-  ssl: {
-    rejectUnauthorized: false, // 🔥 OBRIGATÓRIO para Supabase no Render
+  // 🔥 FORÇA IPv4 + SSL (RESOLVE O ENETUNREACH)
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    family: 4, // 👈 ISSO resolve o IPv6
   },
 
-  synchronize: true, // ⚠️ só enquanto estiver desenvolvendo
+  synchronize: true,
   logging: false,
   entities: [User, Task],
 });
